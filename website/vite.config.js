301ecd807db06ff5575b34d6a81232a729b64665
Filippo Vicentini <filippovicentini@gmail.com>
2025-07-06 23:58:21 +0200

    fix: point asset base at /speed/ for GH Pages

--- website/vite.config.js
+++ website/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // tell Vite to prefix all asset URLs with '/speed/'
+  base: '/speed/',
   plugins: [react()],
   build: {
-    outDir: 'docs' // so GitHub Pages can serve from /docs
+    outDir: 'docs'
   }
 })
